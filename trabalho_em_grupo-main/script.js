5281badcdc8de130da51af185632b1a6375f9263
luiseduardolima2009 <luis.moura.lima@escola.pr.gov.br>
2025-08-14 11:57:23 -0300

    Add files via upload

---
+++ trabalho_em_grupo-main/script.js
@@ -0,0 +1,30 @@
+const hospedes = [
+
+{ id: "numero_um", nome: "um", preco: 200},
+{ id: "numero_tres", nome: "tres", preco: 400},    
+{ id: "numero_seis", nome: "seis", preco: 600}                                        
+
+]
+
+const quartos = [
+
+{ id: "quartos", nome: "luxo", preco: 1500},
+{ id: "quartos", nome: "medio", preco: 550},
+{ id: "quartos", nome: "barato", preco: 300}
+
+
+ ]
+
+const refeicoes = [
+
+{ id: "cafe", nome: "cafe da manha", preco: 150},
+{ id: "quartos", nome: "almoco", preco: 125},
+{ id: "jantar", nome: "jantar", preco: 100},
+
+]
+
+function calcula(){
+
+
+    
+}
