65b79e4c9048aecc0943910dcecaeeb713727518
luiseduardolima2009 <luis.moura.lima@escola.pr.gov.br>
2025-08-14 08:46:12 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+const hospedes = [
+
+{ id: "numero_um", nome: "um", preco: 200},
+{ id: "numero_tres", nome: "tres", preco: 400},    
+{ id: "numero_seis", nome: "seis", preco: 600}                                        
+
+]
+
+const quartos = [
+
+{ id: "quartos", nome: "luxo", preco: 1500},
+{ id: "quartos", nome: "medio", preco: 550},
+{ id: "quartos", nome: "barato", preco: 300}
+
+
+    ]
+
+const refeicoes = [
+
+{ id: "quartos", nome: "luxo", preco: 1500},
+{ id: "quartos", nome: "medio", preco: 550},
+{ id: "quartos", nome: "barato", preco: 300}
+
+]
+
